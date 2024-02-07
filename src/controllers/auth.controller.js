8990064d04091a0f3fbc44b6a273759648792a3b
Franco <francozeta2011@gmail.com>
2024-02-07 18:54:33 -0500

    Added controllers for registration, login, and logout routes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -32,7 +32,48 @@ export const register = async (req, res) => {
 	} catch (error) {
 		res.status(500).json({message: error.message});
 	}
+};
+export const login = async (req, res) => { 
+	//req.body seran los datos que el cliente envie
+	const { email, password } = req.body;
+
+	try {
+		const userFound = await User.findOne({email});
+		if(!userFound) {
+			// Si el usuario no se encontro en la base de datos
+			return res.status(400).json({message: "User not found"});
+		};
+
+		/* - Compara la contraseña ingresada por el usuario con la contraseña encriptada almacenada en la base de datos.
+		- Utiliza la función bcrypt.compare() para verificar si coinciden. */
+		const isMatch = await bcrypt.compare(password, userFound.password);
+		if(!isMatch) {
+			return res.status(400).json({ message: "Incorrect password"});
+		}
+
+
+
+		const token = await createAccessToken({id: userFound.id})
+		res.cookie('token', token);
+		res.json({
+			id: userFound.id,
+			username: userFound.username,
+			email: userFound.email,
+			createdAt: userFound.createdAt,
+			updatedAt: userFound.updatedAt
+		});
+
+
+		// res.send('Registrando...')
+	} catch (error) {
+		res.status(500).json({message: error.message});
+	}
 
 
 };
-export const login = (req, res) => {};
+export const logout = (req, res) => {
+	res.cookie('token', "", {
+		expires: new Date(0)
+	});
+	res.sendStatus(200);
+};	
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { register, login } from '../controllers/auth.controller.js'
+import { register, login, logout } from '../controllers/auth.controller.js'
 
 const router = Router();
 
 // Creamos rutas
 router.post('/register', register);
 router.post('/login', login);
+router.post('/logout', logout);
 
 
 export default router;

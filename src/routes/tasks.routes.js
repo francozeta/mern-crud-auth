0fb173206674d4036513477fb6fc4440232958dd
Franco <francozeta2011@gmail.com>
2024-02-12 15:16:07 -0500

    Added user authentication middleware

---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,12 @@
+import { Router	 } from 'express';
+import { authRequired } from '../middlewares/validateToken.js';
+
+
+const router = Router();
+
+router.get('/tasks', authRequired, (req, res) => {
+	res.send('Task here')
+})
+
+
+export default router;

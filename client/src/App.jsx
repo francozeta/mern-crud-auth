5cf22c436c1b5380016a8bf687e48117196c1ac9
Franco <francozeta2011@gmail.com>
2024-02-14 10:44:41 -0500

    Created IU to user registry

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+
 function App() {
 	return (
 		<BrowserRouter>
 		<Routes>
 			<Route path='/' element={<h1 className='text-4xl font-bold'>Hello Page</h1>}/>
-			<Route path='/login' element={<h1 className='text-4xl font-bold'>Login</h1>}/>
-			<Route path='/register' element={<h1 className='text-4xl font-bold'>Register</h1>}/>
+			<Route path='/login' element={<LoginPage/>}/>
+			<Route path='/register' element={<RegisterPage/>}/>
 			<Route path='/tasks' element={<h1 className='text-4xl font-bold'>Tasks Page</h1>}/>
 			<Route path='/add-task' element={<h1 className='text-4xl font-bold'>Add Task</h1>}/>
 			<Route path='/task/:id' element={<h1 className='text-4xl font-bold'>Update Task</h1>}/>
--- src/app.js
+++ src/app.js
@@ -3,16 +3,18 @@ import morgan from 'morgan';
 import authRoutes from './routes/auth.routes.js';
 import authRoutesTasks from './routes/tasks.routes.js';
 import cookieParser from 'cookie-parser';
-
-
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors({
+	origin: 'http://localhost:5173' //solo en este dominio se podra comunicar el backend
+}));
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json());	
 app.use(cookieParser());
 app.use('/api', authRoutes);
-app.use('/api', authRoutesTasks)
+app.use('/api', authRoutesTasks);
 
 
 export default app;
